5cfa32b93203083a298600a42426f0bcbf7e1501
Julian Kruchy <julian.kruchy@tu-dresden.de>
2020-09-10 08:17:09 +0200

    Fixed a Bug

--- tests/type_test.py
+++ tests/type_test.py
@@ -1,7 +1,7 @@
 import pytest
 from math import pow
 
-from ..metricq.types import Timedelta
+from metricq.types import Timedelta
 
 
 @pytest.fixture
@@ -29,24 +29,21 @@ def time_delta_generate_random_list(count=1000):
     mod = 9987868812
 
     for _ in range(count):
-        l.append(Timedelta(v // 20 * pow(10, v % 20)))
+        l.append(Timedelta(int(v // 20 * pow(10, v % 20))))
         v = (v * mult + increase) % mod
 
     return l
 
 
-def test_timedelta_to_string():
-    td1 = time_delta_random()
-    td2 = time_delta_1s()
-    td3 = time_delta_1d()
+def test_timedelta_to_string(time_delta_random, time_delta_1s, time_delta_1d):
 
-    assert td1.precise_string == "8295638928ns"
-    assert td2.precise_string == "1s"
-    assert td3.precise_string == "1d"
+    assert time_delta_random.precise_string == "8295638928ns"
+    assert time_delta_1s.precise_string == "1s"
+    assert time_delta_1d.precise_string == "1d"
 
 
-def test_timedelta_to_string_and_reverse():
+def test_timedelta_to_string_and_reverse(time_delta_generate_random_list):
 
-    for t in time_delta_generate_random_list():
+    for t in time_delta_generate_random_list:
         assert Timedelta.from_string(t.precise_string) == t
 
