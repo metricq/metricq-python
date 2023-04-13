20dc19d80a8dd886f5cf18a46812fd49e27e26a5
Thomas Ilsche <thomas.ilsche@tu-dresden.de>
2023-04-13 14:21:22 +0200

    refactor: move extra types to extras module

---
+++ metricq/timeseries/extras.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+Metric = str
+"""Type alias for strings that represent metric names"""
+
+JsonDict = dict[str, Any]
+"""Type alias for dicts that represent JSON data"""
+
+MetadataDict = JsonDict
+"""Type alias for a dict containing metric metadata"""
