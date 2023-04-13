ea82f0c8780f9bb2c4916c2f48d43e9f421d4602
Thomas Ilsche <thomas.ilsche@tu-dresden.de>
2023-04-13 14:21:22 +0200

    refactor: move extra types to extras module

--- metricq/timeseries/__init__.py
+++ metricq/timeseries/__init__.py
@@ -1,18 +1,8 @@
-from typing import Any
-
+from .extras import JsonDict, MetadataDict, Metric
 from .time_aggregate import TimeAggregate
 from .time_value import TimeValue
 from .timedelta import Timedelta
 from .timestamp import Timestamp
-
-Metric = str
-"""Type alias for strings that represent metric names"""
-
-JsonDict = dict[str, Any]
-"""Type alias for dicts that represent JSON data"""
-
-MetadataDict = JsonDict
-"""Type alias for a dict containing metric metadata"""
 
 __all__ = [
     "TimeAggregate",
